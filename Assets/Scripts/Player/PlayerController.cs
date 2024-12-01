0a7affb6cafdb83b229aa9a5b8d2b9f4cde75942
unknown <puhalenthirv@gmail.com>
2024-12-01 10:14:45 -0500

    add rigidbodies/colliders and improve player movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,42 +6,53 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject player;
 
-    [SerializeField]
-    private float playerSpeed = 6.0f;
+    
+    private float playerWalkingSpeed = 2.0f;
+    private float playerCrouchingSpeed = 1.0f;
+
+    private Rigidbody playerRigidbody;
 
     private bool isCrouching = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidbody = player.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Keyboard Inputs
-        if (Input.GetKey(KeyCode.W))
-        {
-            // move the player relative to the game object
-            player.transform.Translate(Vector3.forward * playerSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            player.transform.Translate(Vector3.back * playerSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            player.transform.Translate(Vector3.left * playerSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            player.transform.Translate(Vector3.right * playerSpeed * Time.deltaTime);
-        }
         if (Input.GetKeyDown(KeyCode.H))
         {
             ToggleCrouch();
         }
+
+    }
+    void FixedUpdate()
+    {
+        float speed = isCrouching ? playerCrouchingSpeed : playerWalkingSpeed;
+        Vector3 movement = Vector3.zero;
+
+        // Keyboard Inputs
+        if (Input.GetKey(KeyCode.W))
+        {
+            movement += player.transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            movement += -player.transform.forward;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            movement += -player.transform.right;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            movement += player.transform.right;
+        }
+
+        playerRigidbody.MovePosition(player.transform.position + movement * Time.deltaTime * speed);
     }
 
     void ToggleCrouch()
